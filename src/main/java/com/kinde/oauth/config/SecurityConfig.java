88a9298edb170375e6c852e34e906838a1de5c5f
Koman Rudden <komanrudden@gmail.com>
2024-09-03 07:18:03 +0200

    Page styling customised to Kinde

--- src/main/java/com/kinde/oauth/config/SecurityConfig.java
+++ src/main/java/com/kinde/oauth/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.kinde.oauth.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -14,12 +19,21 @@ import java.util.stream.Collectors;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@Slf4j
 public class SecurityConfig {
+
+    private final LogoutHandler customLogoutHandler;
+
+    public SecurityConfig(CustomLogoutHandler customLogoutHandler) {
+        this.customLogoutHandler = customLogoutHandler;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**").permitAll()
                         .requestMatchers("/login/**", "/home").permitAll()
                         .anyRequest().authenticated()
                 )
@@ -31,6 +45,7 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/home")
+                        .addLogoutHandler(customLogoutHandler)
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                         .clearAuthentication(true)
@@ -39,6 +54,34 @@ public class SecurityConfig {
         return http.build();
     }
 
+    /**
+     * Configures a {@link JwtAuthenticationConverter} to extract and convert the "permissions" claim
+     * from a {@link Jwt} token into a collection of {@link GrantedAuthority} objects.
+     *
+     * <p>This method is invoked during the JWT authentication process in a Spring Security
+     * application. Specifically, it runs when a JWT is being decoded and validated as part
+     * of the authentication process, converting the permissions contained in the JWT into
+     * appropriate roles recognized by the application.</p>
+     *
+     * <p>The {@link JwtAuthenticationConverter} created by this method will:
+     * <ul>
+     *   <li>Extract the "permissions" claim from the JWT.</li>
+     *   <li>Map each permission to a corresponding {@link SimpleGrantedAuthority}:
+     *     <ul>
+     *       <li>"admin" is mapped to "ROLE_ADMIN".</li>
+     *       <li>"read" is mapped to "ROLE_USER".</li>
+     *     </ul>
+     *   </li>
+     *   <li>Ignore any permissions that do not match the expected values.</li>
+     * </ul>
+     * </p>
+     *
+     * <p>This method is typically used in a {@link SecurityConfigurerAdapter} to customize
+     * the JWT authentication process, ensuring that users are granted the correct authorities
+     * based on the claims present in their JWT.</p>
+     *
+     * @return a configured {@link JwtAuthenticationConverter} that maps JWT permissions to Spring Security roles.
+     */
     private JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
         converter.setJwtGrantedAuthoritiesConverter(jwt -> jwt.getClaimAsStringList("permissions")
--- src/main/java/com/kinde/oauth/controller/KindeController.java
+++ src/main/java/com/kinde/oauth/controller/KindeController.java
@@ -1,33 +1,45 @@
 package com.kinde.oauth.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.kinde.KindeClient;
+import com.kinde.KindeClientBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.util.Map;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @Controller
+@Slf4j
 public class KindeController {
+
+    private final WebClient webClient;
 
     private final OAuth2AuthorizedClientService authorizedClientService;
 
-    public KindeController(OAuth2AuthorizedClientService authorizedClientService) {
+    public KindeController(WebClient webClient, OAuth2AuthorizedClientService authorizedClientService) {
+        this.webClient = webClient;
         this.authorizedClientService = authorizedClientService;
     }
 
-    @GetMapping("/api/auth/kinde_callback")
-    public String callback(@AuthenticationPrincipal OidcUser principal) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication.getPrincipal() instanceof OidcUser) {
-            OidcUser oidcUser = ((OidcUser) authentication.getPrincipal());
+    @GetMapping(path = "/dashboard")
+    public String dashboard(Model model) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof DefaultOidcUser user) {
+            model.addAttribute("fullName", user.getUserInfo().getFullName());
+            model.addAttribute("token", user.getIdToken().getTokenValue());
         }
-        return "user";
+        return "dashboard";
     }
 
     @GetMapping(path = "/home")
@@ -39,23 +51,52 @@ public class KindeController {
         return "home";
     }
 
-    @GetMapping(path = "/public")
-    public String index(Model model) {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof DefaultOidcUser user) {
-            model.addAttribute("username", user.getUserInfo().getFullName());
-        }
-        return "public";
+    @GetMapping(path = "/userprofile")
+    public String logout(Model model,
+                         @RegisteredOAuth2AuthorizedClient("kinde") OAuth2AuthorizedClient authorizedClient) {
+
+        // @formatter:off
+        String userprofile = this.webClient
+                .get()
+                .attributes(oauth2AuthorizedClient(authorizedClient))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        // @formatter:on
+        model.addAttribute("userprofile", userprofile);
+        return "userprofile";
     }
 
-    @GetMapping(path = "/landing")
-    public String landing(Model model, OAuth2AuthenticationToken authentication) {
+    @GetMapping(path = "/properties")
+    public String properties(Model model, OAuth2AuthenticationToken authentication) {
+        OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(
+                authentication.getAuthorizedClientRegistrationId(),
+                authentication.getName());
+
+        String accessToken = authorizedClient.getAccessToken().getTokenValue();
+
+        KindeClient kindeClient = KindeClientBuilder
+                .builder()
+                .domain("https://koman.kinde.com")
+                .clientId("a06a72f6df3642fe85e99e4084ddf866")
+                .clientSecret("Ts3GjhZrDomohSMGDzhzjOgiK1SYXo7ZINzd73B6qywMBOoT8viHq")
+                .redirectUri("http://localhost:8081")
+                .addScope("openid")
+                .build();
+        model.addAttribute("userinfo", kindeClient.clientSession().retrieveUserInfo().getUserInfo());
+
+        return "api";
+    }
+
+    @GetMapping(path = "/api")
+    public String api(Model model, OAuth2AuthenticationToken authentication) {
         OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(
                 authentication.getAuthorizedClientRegistrationId(),
                 authentication.getName());
 
         String accessToken = authorizedClient.getAccessToken().getTokenValue();
         model.addAttribute("access_token", accessToken);
-        return "landing";
+
+        return "api";
     }
 }
